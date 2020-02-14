ff46050b418a2261b423a9b6cb65afc75fde00e6
matsarou <matsarou@hotmail.com>
2020-02-14 11:08:15 +0200

    add some songs and modifications on classifier4

--- correlations/Classifier.py
+++ correlations/Classifier.py
@@ -3,8 +3,8 @@ import numpy as np
 import sys
 import argparse
 from sklearn import preprocessing
-from sklearn.metrics import accuracy_score, precision_recall_curve, auc
-from sklearn import model_selection, metrics
+from sklearn.metrics import accuracy_score
+from sklearn import model_selection
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import confusion_matrix
 import seaborn as sn
@@ -18,16 +18,16 @@ def main(platform='spotify', users=[]):
     df = preprocess_datasheet(df, users)
     if(platform == 'spotify'):
         Y_predicted, Y_real = spotify_generate_predictions(df)
-        if (str(users) == ['0']):
+        if(allUsersAskedFor(users)):
             plot_confusion_matrix(Y_real, Y_predicted, "heatmap_allUsers")
-        if(len(users) > 0):
+        elif(particularUsersAskedFor(users)):
             plot_confusion_matrix(Y_real, Y_predicted, "heatmap_users"+str(users))
     elif(platform == 'youtube'):
         print("Add youtube classifier")
 
 def preprocess_datasheet(df, users=[]):
     df['user_id'] = df['user_id'].astype(int).astype(str)
-    if (len(users) > 0 and users[0] != '0'):
+    if (particularUsersAskedFor(users)):
         # ask for correlations of specific user
         df = df[df['user_id'].isin(users)]
     df = df.dropna(axis=0)
@@ -37,15 +37,17 @@ def preprocess_datasheet(df, users=[]):
     return df
 
 def spotify_generate_predictions(df):
-    X = df.take([1,5,6,9,10,12,18,10], axis=1)  # predictors
+    X = df.take([1,5,6,9,10,12,18,21], axis=1)  # predictors
     X = X.apply(pd.to_numeric)
     X = X.iloc[1:]
 
-    df['Class'] = df['mood'] + "_at_" + df['location'] + "_while_" + df['activity']
+    df = unify_classes(df)
+    df['Class'] = df['mood'] + "_at_" + df['location'] + "_while_" + df['activity_unified']
+    Y = df['Class']  # predicted_class
+    Y = Y.iloc[1:]
+
     classes = np.unique(df['Class'].values)
     print("We have {} unique classes: {}".format(len(classes), classes))
-    Y = df['Class'] # predicted_class
-    Y = Y.iloc[1:]
 
     seed = 7
     num_trees = 30
@@ -58,16 +60,31 @@ def spotify_generate_predictions(df):
 
     return predictions, Y
 
+
+def unify_classes(df):
+    df['mood'] = df['mood'].replace({'angry': 'unhappy', 'sad': 'unhappy', 'nervous': 'unhappy'})
+    df['mood'] = df['mood'].replace({'bored': 'annoyed', 'sleepy': 'annoyed'})
+    df['mood'] = df['mood'].replace({'calm': 'relaxed', 'relaxed': 'relaxed', 'peaceful': 'relaxed'})
+    df['mood'] = df['mood'].replace({'excited': 'happy', 'happy': 'happy', 'pleased': 'happy'})
+    df['activity_unified'] = df['activity']
+    df['activity_unified'] = df['activity_unified'].replace({'commuting': 'other'})
+    return df
+
+def allUsersAskedFor(users=['0']):
+    return len(users) > 0 and users[0] == '0'
+
+def particularUsersAskedFor(users=['0']):
+    return len(users) > 0 and users[0] != '0'
+
 def plot_confusion_matrix(Y, predictions,name):
     cf_matrix = confusion_matrix(predictions, Y.values.ravel())
     classes = np.unique(Y.values)
-    fig = plt.figure(figsize=(20, 20))
+    fig = plt.figure(figsize=(25, 25)) #figsize=(20, 20)
     mood_matx = sn.heatmap(cf_matrix / np.sum(cf_matrix), annot=True, xticklabels=classes, yticklabels=classes,
                            fmt='.2%', cmap='Reds')
-    mood_matx.set_xticklabels(mood_matx.get_xticklabels(), rotation=25)
+    mood_matx.set_xticklabels(mood_matx.get_xticklabels(), rotation=90)
     plt.xlabel('Predicted Class')
     plt.ylabel('Real Class')
-    # fig = mood_matx.get_figure()
     fig.savefig("correlations/figures/"+name+".png")
 
 # Predict regarding all users: Classifier.py -p spotify
